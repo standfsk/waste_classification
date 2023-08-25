e32c4ddffc37ac7729bac581d85d0edad55ac54f
cshyo1004 <cshyo1004@gmail.com>
2023-08-25 20:37:32 +0900

    데이터 전처리 과정 추가 및 코드 정리

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 import json
-import cv2
 import numpy as np
+import pandas as pd
 
 # 값이 디렉터리 인지 확인
 def check_dir(path):
@@ -13,10 +13,16 @@ def list_dir(path):
     return [x for x in os.listdir(os.path.join(path)) if check_dir(os.path.join(path, x))]
 
 # 파일 이동
-def move_files(src, dest):
-    files = [x for x in os.listdir(src) if not os.path.isdir(os.path.join(src, x))]
+def move_files(src, dest, exts):
+    files = [x for x in os.listdir(src) if x.split('.')[-1] in (exts['image_ext'] + exts['json_ext'])]
     for f in files:
         shutil.move(os.path.join(src, f), os.path.join(dest, f))
+
+# 파일 복사
+def copy_files(src, dest, exts):
+    files = [x for x in os.listdir(src) if x.split('.')[-1] in (exts['image_ext'] + exts['json_ext'])]
+    for f in files:
+        shutil.copy(os.path.join(src, f), os.path.join(dest, f))
 
 # 클래스 정보에 해당하지 않는 폴더들 삭제
 def folders_by_classes(path, classes):
@@ -24,12 +30,12 @@ def folders_by_classes(path, classes):
         shutil.rmtree(os.path.join(path, dir))
 
 # 디렉터리별 파일을 /data로 이동
-def gather_data(path, data_path):
+def gather_data(path, data_path, exts):
     if list_dir(path):
         for path2 in list_dir(path):
             gather_data(os.path.join(path, path2))
     else:
-        move_files(path, data_path)
+        move_files(path, data_path, exts)
 
 # r/g/b -> hexadecimal
 def rgb_to_hex(r, g, b):
@@ -55,11 +61,9 @@ def create_folders(path):
     os.makedirs(os.path.join(val_path, 'labels'))
     os.makedirs(os.path.join(test_path, 'images'))
     os.makedirs(os.path.join(test_path, 'labels'))
-    os.mkdir(os.path.join(path, 'jsons'))
-    os.mkdir(os.path.join(path, 'imgs'))
 
 # 디렉터리별로 데이터 분배
-def move_by_dirs(dir, path, files):
+def move_by_dirs(path, dir, files):
     img_path = os.path.join(path, dir, 'images')
     lbl_path = os.path.join(path, dir, 'labels')
     for f in files:
@@ -116,3 +120,125 @@ def normalize_image(image):
 
     normalized_image = np.stack((normalized_r, normalized_g, normalized_b), axis=-1)
     return normalized_image
+
+# 이미지 속성 값 추출 -> csv
+def extract_imgattr(path, translation, headers):
+    files = set([x.split('.')[0] for x in os.listdir(path) if x.endswith('.Json')])
+
+    image_properties = {}
+    removed_files = {}
+    for f in files:
+        values = []
+        errors = []
+        # json 파일 불러오기
+        with open(os.path.join(path, f+'.Json'), 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+        # 객체 1개가 아닐경우, 파일 삭제
+        if data['BoundingCount'] != '1':
+            errors.append('multiple object detected')
+            removed_files[f] = errors
+            os.remove(os.path.join(path, f+'.Json'))
+            os.remove(os.path.join(path, f+'.jpg'))
+            continue
+        # 객체 1개일 경우, 필요 이미지 속성 값 추출하여 저장
+        else:
+            if data['Bounding'][0]['Drawing'] == 'BOX':
+                # values.append(data['MAKE'])
+                # values.append(data['Camera Model Name'])
+                values.append(resolution_preprocess(data['RESOLUTION']))
+                values.append(int(data['Sensitivity iso']))
+                values.append(translation[data['DAY/NIGHT']])
+                values.append(translation[data['PLACE']])
+                values.append(translation[data['Bounding'][0]['CLASS']])
+                obj_color = list(map(int, data['Bounding'][0]['Color'].split('/')))
+                values.append(rgb_to_hex(*obj_color))
+                values.append(data['Bounding'][0]['x1'])
+                values.append(data['Bounding'][0]['y1'])
+                values.append(data['Bounding'][0]['x2'])
+                values.append(data['Bounding'][0]['y2'])
+            else:
+                errors.append('polygon label detected')
+                removed_files[f] = errors
+                os.remove(os.path.join(path, f+'.Json'))
+                os.remove(os.path.join(path, f+'.jpg'))
+                continue
+        image_properties[f] = values
+
+    # 이미지별 속성값 csv파일로 저장
+    df = pd.DataFrame.from_dict(image_properties, orient='index', columns=headers)
+    df.to_csv(os.path.join(path, 'data1.csv'), encoding='cp949')
+
+    # 제거 이미지 및 제거 이유 csv파일로 저장
+    df2 = pd.DataFrame.from_dict(removed_files, orient='index', columns=['reason'])
+    df2.to_csv(os.path.join(path, 'removed_data1.csv'), encoding='cp949')
+
+# 필요없는 데이터 제거
+def remove_unmatched(path, exts):
+    from collections import Counter
+    files = Counter([x.split('.')[0] for x in os.listdir(path) if x.split('.')[-1] in (exts['image_ext'] + exts['json_ext'])])
+    unmatched_files = [key for key, value in files.items() if value == 1]
+    
+    for f in unmatched_files:
+        try:
+            os.remove(os.path.join(path, f+'.jpg'))
+        except:
+            os.remove(os.path.join(path, f+'.Json'))
+
+# json 파일의 바운딩 박스 값을 yolo format으로 변환
+def cvt2YOLO(row):
+    img_size = list(map(int, row['resolution'].values[0].split('*')))
+    x_min = row['x_min'].values[0]
+    y_min = row['y_min'].values[0]
+    x_max = row['x_max'].values[0]
+    y_max = row['y_max'].values[0]
+    bbox = [x_min, y_min, x_max, y_max]
+    obj_class = row['obj_class'].values[0]
+    x,y,w,h = bboxtoyoloformat(img_size, bbox)
+    return [obj_class, x, y, w, h]
+    
+# 데이터 전처리
+def data_preprocess(path, classes_to_code, img_size, exts):
+    from PIL import Image
+    import cv2
+
+    # csv파일 정보를 기반으로 텍스트 파일 생성 // img_size, bbox,  obj_class
+    files = set([x.split('.')[0] for x in os.listdir(path) if x.split('.')[-1] in (exts['image_ext'] + exts['json_ext'])])
+    for f in files:
+        image_file = f + '.jpg'
+        
+        df = pd.read_csv(os.path.join(path, 'data1.csv'))
+        row = df[df.iloc[:,0] == f]
+        # json -> txt
+        YOLO_values = cvt2YOLO(row)
+        with open(f'{os.path.join(path, f)}.txt', 'w') as txtfile:
+            txtfile.write(f'{classes_to_code[YOLO_values[0]]} {YOLO_values[1]} {YOLO_values[2]} {YOLO_values[3]} {YOLO_values[4]}')
+        # 이미지 데이터 resize
+        img = Image.open(os.path.join(path, image_file))
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+        resized_img = cv2.resize(img, dsize=(img_size), interpolation=cv2.INTER_LINEAR)
+        cv2.imwrite(os.path.join(path, image_file), resized_img)
+        # 라벨 데이터 전처리
+        bbox_resized = resize_bbox(img, resized_img, os.path.join(path, image_file.split('.')[0]+'.json'))
+        change_bbox(bbox_resized, os.path.join(path, image_file.split('.')[0]+'.json'), os.path.join(path, image_file.split('.')[0]+'.json'))
+        # # 이미지 데이터 표준화
+        # normalized_image = normalize_image(resized_img)
+        
+        # 라벨 데이터 제거
+        os.remove(os.path.join(path, f+'.Json'))
+
+# 데이터 분배 (8:1:1)
+def distribute_files(path, exts):
+    import numpy as np
+    files = [x for x in os.listdir(path) if x.split('.')[-1] in exts['image_ext']]
+    train_files, val_files, test_files = np.split(np.array(files), [int(len(files)*0.8), int(len(files)*0.9)])
+    all_files = {'train': train_files, 'val': val_files, 'test': test_files}
+    for dir, file_list in all_files.items():
+        move_by_dirs(path, dir, file_list)
+
+# 슬랙 봇에 메시지 보내기
+def end_message(token, text):
+    from slackbot_run import SlackAPI
+    slack = SlackAPI(token)
+    channel_name = 'slackbot_'
+    channel_id = slack.get_channel_id(channel_name)
+    slack.post_message(channel_id, text)
--- main.py
+++ main.py
@@ -2,13 +2,8 @@
 import data_preprocessing
 
 # python-library
-import shutil
 import os
-import json
-import pandas as pd
-import numpy as np
-import cv2
-import ast
+
 
 if __name__ == '__main__':
     # 데이터셋 경로 설정
@@ -16,14 +11,15 @@ if __name__ == '__main__':
 
     # 데이터 저장 경로 설정
     data_path = 'data'
+    backup_path = 'backup'
 
     # 클래스 정보 딕셔너리
     classes = {'15': '비닐', '17': '유리병', '21': '종이류', '22': '캔류', '23': '페트병', '24': '플라스틱류'}
     classes_translation = {'비닐': 'vinyl', '유리병': 'glass', '종이류': 'paper', '캔류': 'can', '페트병': 'PET', '플라스틱류': 'plastic'}
     classes_to_code = {'vinyl': '15', 'glass': '17', 'paper': '21', 'can': '22', 'PET': '23', 'plastic': '24'}
 
-    # 이미지 속성 딕셔너리
-    translation = {
+    # 이미지 속성 번역 딕셔너리
+    imgattr_translation = {
         '주간': 'day',
         '야간': 'night',
         '실내': 'indoor',
@@ -38,139 +34,53 @@ if __name__ == '__main__':
     }
 
     # 필요 이미지 속성 리스트
-    headers = ['MAKE', 'Camera Model Name', 'resolution', 'iso', 'daynight', 'place', 'obj_class', 'obj_color', 'bbox']
+    imgattr_headers = ['resolution', 'iso', 'daynight', 'place', 'obj_class', 'obj_color', 'x_min', 'y_min', 'x_max', 'y_max']
 
     # 확장자
-    image_ext = ['jpg', 'png']
-    json_ext = ['json', 'Json']
+    exts = {
+        'image_ext': ['jpg', 'png'],
+        'json_ext': ['json', 'Json']
+    }
 
     # 이미지 사이즈
     img_size = (640, 640)
+
+    # 백업 디렉터리가 없을 경우, 생성
+    if not os.path.isdir(backup_path):
+        os.mkdir(backup_path)
 
     # # 라벨링 데이터 내에 필요없는 클래스의 자료들 삭제
     # data_preprocessing.folders_by_classes(f'{dataset_path}/Training/Training_라벨링데이터')
     # data_preprocessing.folders_by_classes(f'{dataset_path}/Validation/[V라벨링]라벨링데이터')
 
     # # 전체 파일 이동 // dataset_path -> data_path
-    # data_preprocessing.gather_data(dataset_path, data_path)
-
-    # 누락 데이터 제거
-    # 1차 json 파일 리스트
-    json_files = [x for x in os.listdir(data_path) if x.split('.')[-1] in json_ext]
-
-    # 이미지 속성 값 추출
-    image_properties = {}
-    removed_files = {}
-    for f in json_files:
-        values = []
-        values_lst = []
-        errors = []
-        # json 파일 불러오기
-        with open(os.path.join(data_path, f), 'r', encoding='utf-8') as json_file:
-            data = json.load(json_file)
-        # 객체 1개가 아닐경우, 파일 삭제
-        if data['BoundingCount'] != '1':
-            errors.append('multiple object detected')
-            removed_files[f] = errors
-            os.remove(os.path.join(data_path, f))
-            continue   
-        # 객체 1개일 경우, 필요 이미지 속성 값 추출하여 저장
-        else:
-            if data['Bounding'][0]['Drawing'] == 'BOX':
-                values.append(data['MAKE'])
-                values.append(data['Camera Model Name'])
-                values.append(data_preprocessing.resolution_preprocess(data['RESOLUTION']))
-                values.append(int(data['Sensitivity iso']))
-                values.append(translation[data['DAY/NIGHT']])
-                values.append(translation[data['PLACE']])
-                values.append(translation[data['Bounding'][0]['CLASS']])
-                obj_color = list(map(int,data['Bounding'][0]['Color'].split('/')))
-                values.append(data_preprocessing.rgb_to_hex(*obj_color))
-                values_lst.append(data['Bounding'][0]['x1'])
-                values_lst.append(data['Bounding'][0]['y1'])
-                values_lst.append(data['Bounding'][0]['x2'])
-                values_lst.append(data['Bounding'][0]['y2'])
-                values.append(values_lst)
-            else:
-                errors.append('polygon label detected')
-                removed_files[f] = errors
-                os.remove(os.path.join(data_path, f))
-                continue
-        image_properties[f] = values
-
-    # 이미지 속성값 csv파일로 저장
-    df = pd.DataFrame.from_dict(image_properties, orient='index', columns=headers)
-    df.to_csv(os.path.join('data1.csv'), encoding='cp949')
-
-    # 제거할 이미지 csv파일로 저장
-    df2 = pd.DataFrame.from_dict(removed_files, orient='index', columns=['reason'])
-    df2.to_csv(os.path.join('removed_data1.csv'), encoding='cp949')
-
-    # 파일 리스트
-    files = [x for x in os.listdir(data_path) if x.split('.')[-1] in (image_ext + json_ext)]
-    image_files = [x.split('.')[0] for x in files if x.split('.')[-1] in image_ext]
-    json_files = [x.split('.')[0] for x in files if x.split('.')[-1] in json_ext]
-
-    # 대칭 차집합 // json 파일만 있거나 image 파일만 있는 데이터 리스트
-    unused = set(json_files) ^ set(image_files)
+    # data_preprocessing.gather_data(dataset_path, data_path, exts)
 
     # 필요없는 데이터 제거
-    for f in files:
-        ftype = f.split('.')[-1]
-        if f.split('.')[0] in unused:
-            os.remove(os.path.join(data_path, f))
+    data_preprocessing.remove_unmatched(data_path, exts)
+    print("필요없는 데이터 제거 완료")
 
-    # 디렉터리가 없을 경우, 생성
+    # 이미지 속성 값 추출
+    data_preprocessing.extract_imgattr(data_path, imgattr_translation, imgattr_headers)
+    print("csv파일 생성 완료")
+
+    # 백업 파일
+    data_preprocessing.copy_files(data_path, backup_path, exts)
+    print("백업 파일 저장 완료")
+
+    # 서브 디렉터리가 없을 경우, 생성
     if not os.path.isdir(os.path.join(data_path, 'train')):
         data_preprocessing.create_folders(data_path)
 
-    # csv파일 정보를 기반으로 텍스트 파일 생성 // img_size, bbox,  obj_class
-    data_df = pd.read_csv('data1.csv')
-    j_fs = [x for x in os.listdir(data_path) if x.split('.')[-1] in json_ext]
-    for j_f in j_fs:
-        row = data_df[data_df.iloc[:,0] == j_f]
-        img_size = list(map(int, row['resolution'].values[0].split('*')))
-        x1, y1, x2, y2 = list(map(int, ast.literal_eval(row['bbox'].values[0])))
-        bbox = [x1, x2, y1, y2]
-        obj_class = row['obj_class'].values[0]
-        x,y,w,h = data_preprocessing.bboxtoyoloformat(img_size, bbox)
-        with open(f'{os.path.join(data_path, j_f.split(".")[0])}.txt', 'w') as txtfile:
-            txtfile.write(f'{classes_to_code[obj_class]} {x} {y} {w} {h}')
-
     # 데이터 전처리
-    json_files = [x for x in os.listdir(data_path) if x.split('.')[-1] in json_ext]
-    image_files = [x for x in os.listdir(data_path) if x.split('.')[-1] in image_ext]
-    for image_file in image_files:
-        # 이미지 데이터 복사
-        shutil.copy(os.path.join(data_path, image_file), os.path.join(data_path, image_file.split('.')[0] + '_copy.jpg'))
-        # 이미지 데이터 resize
-        img = cv2.imread(os.path.join(data_path, image_file))
-        resized_img = cv2.resize(img, dsize=(img_size), interpolation=cv2.INTER_LINEAR)
-        cv2.imwrite(os.path.join(data_path, image_file), resized_img)
-        # 라벨 데이터 전처리
-        bbox_resized = data_preprocessing.resize_bbox(img, resized_img, os.path.join(data_path, image_file.split('.')[0]+'.json'))
-        data_preprocessing.change_bbox(bbox_resized, os.path.join(data_path, image_file.split('.')[0]+'.json'), os.path.join(data_path, image_file.split('.')[0]+'.json'))
-        # 이미지 데이터 표준화
-        normalized_image = data_preprocessing.normalize_image(resized_img)
-        # 원본데이터 imgs 폴더로 이동
-        shutil.move(os.path.join(data_path, image_file.split('.')[0] + '_copy.jpg'), os.path.join(data_path, 'imgs', image_file))
-
-    # 파일을 8:1:1 비율로 분배
-    # 훈련 데이터
-    train_files, val_files, test_files = np.split(np.array(image_files), [int(len(image_files)*0.8), int(len(image_files)*0.9)])
-    all_files = {'train': train_files, 'val': val_files, 'test': test_files}
-    for key, value in all_files.items():
-        data_preprocessing.move_by_dirs(key, data_path, value)
-
-    # 백업 파일 저장
-    for j_f in json_files:
-        shutil.move(os.path.join(data_path, j_f), os.path.join(data_path, 'jsons', j_f))
+    data_preprocessing.data_preprocess(data_path, classes_to_code, img_size, exts)
+    print('데이터 전처리 완료')
+    
+    # 파일 분배
+    data_preprocessing.distribute_files(data_path, exts)
+    print("파일 분배 완료")
 
     # 작업 종료시 슬랙에 메시지 전송
-    from slackbot_run import SlackAPI
     token = 'xoxb-5713411137014-5773768770325-TvV4SRyB8WMOwl7yh3SO69Dx'
-    slack = SlackAPI(token)
-    channel_name = 'slackbot_'
-    text = '데이터 전처리 끝'
-    channel_id = slack.get_channel_id(channel_name)
-    slack.post_message(channel_id, text)
+    text = '작업 종료'
+    data_preprocessing.end_message(token, text)
